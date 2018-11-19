31ddd9377989b52494521019a0b9a332bf6a6b5c
Miguel Angel <mangelmcho@gmail.com>
2018-11-19 16:19:02 -0400

    CRUD usuario

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usuariosRouter = require('./routes/api/usuarios');
 
 const port = process.env.PORT || 3000;
 
@@ -25,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/usuarios', usuariosRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
